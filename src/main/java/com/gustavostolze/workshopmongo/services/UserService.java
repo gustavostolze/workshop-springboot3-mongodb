1bd6b08310ece37c76dedab857e990f91e20a38b
Gustavo <gustavostolzedeandrade@gmail.com>
2025-03-07 15:17:51 -0300

    Obtendo um usuario por id e boas praticas de tratamento de erro

--- src/main/java/com/gustavostolze/workshopmongo/services/UserService.java
+++ src/main/java/com/gustavostolze/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.gustavostolze.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavostolze.workshopmongo.domain.User;
 import com.gustavostolze.workshopmongo.repository.UserRepository;
+import com.gustavostolze.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
