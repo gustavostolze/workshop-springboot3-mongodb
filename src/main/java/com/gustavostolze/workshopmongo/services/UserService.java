65b263bf506dd04b2936b2775275de901cbde4df
Gustavo <gustavostolzedeandrade@gmail.com>
2025-03-07 15:42:02 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/gustavostolze/workshopmongo/services/UserService.java
+++ src/main/java/com/gustavostolze/workshopmongo/services/UserService.java
@@ -25,11 +25,16 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
